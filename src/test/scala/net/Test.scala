c916690927278bf4558bfc7c289c90cdafdbb497
kevinmeredith <kevin.m.meredith@gmail.com>
2016-06-02 16:57:49 -0400

    another example

--- src/test/scala/net/Test.scala
+++ src/test/scala/net/Test.scala
@@ -7,16 +7,29 @@ import org.scalacheck.Gen._
 
 object Test {
   def fooOrBar: Gen[String] = oneOf("foo", "bar")
+
+  def f: Gen[String] = for {
+    a <- Gen.const("")
+    b <- fooOrBar
+  } yield s"$a$b"
+
 }
 
 class Test extends FunSuite with Checkers {
 
-  import Test.fooOrBar
+  import Test._
 
   test("generated String should equal 'foo' or 'bar'") {
     check( org.scalacheck.Prop.forAll(fooOrBar) { x: String =>
-      println("x: " + x)
-      x == "foo" || x == "bar"
+      println("x:" + x)
+      (x == "foo" || x == "bar") && x.length == 3
+    })
+  }
+
+  test("generated f should end with 'foo' or 'bar'") {
+    check( org.scalacheck.Prop.forAll(f) { x: String =>
+      println("x:" + x)
+      x.endsWith("foo") || x.endsWith("bar")
     })
   }
 
